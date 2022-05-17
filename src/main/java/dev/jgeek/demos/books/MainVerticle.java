4b101b6f408cdd3e680e496b292f0dca3ac7289c
jgeek <a.kaliacius@gmail.com>
2022-05-17 23:12:27 +0100

    clean up

--- src/main/java/dev/jgeek/demos/books/MainVerticle.java
+++ src/main/java/dev/jgeek/demos/books/MainVerticle.java
@@ -1,23 +1,12 @@
 package dev.jgeek.demos.books;
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Promise;
+
+import io.reactivex.rxjava3.core.Completable;
+import io.vertx.rxjava3.core.AbstractVerticle;
 
 public class MainVerticle extends AbstractVerticle {
-
   @Override
-  public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888, http -> {
-      if (http.succeeded()) {
-        startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
-      } else {
-        startPromise.fail(http.cause());
-      }
-    });
+  public Completable rxStart() {
+    return super.rxStart();
   }
 }
